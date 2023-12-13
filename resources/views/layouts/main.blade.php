f4af41aac723251408dfefc46dc080d14486cf3a
MafyssDB <hanterhanter599@gmail.com>
2023-12-13 15:42:29 +0700

    update links

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,16 +6,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="{{ asset('build/assets/img/apple-icon.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/img/favicon.ico')}}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/apple-icon.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico')}}">
 
-    <link rel="stylesheet" href="{{ asset('build/assets/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('build/assets/css/templatemo.css')}}">
-    <link rel="stylesheet" href="{{ asset('build/assets/css/custom.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/templatemo.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css')}}">
 
     <!-- Load fonts style after rendering the layout styles -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
-    <link rel="stylesheet" href="{{ asset('build/assets/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css')}}">
     <!--
 
     TemplateMo 559 Zay Shop
@@ -161,11 +161,11 @@
 </footer>
 <!-- End Footer -->
 </body>
-<script src="{{ asset('build/assets/js/jquery-1.11.0.min.js')}}"></script>
-<script src="{{ asset('build/assets/js/jquery-migrate-1.2.1.min.js')}}"></script>
-<script src="{{ asset('build/assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{ asset('build/assets/js/templatemo.js')}}"></script>
-<script src="{{ asset('build/assets/js/custom.js')}}"></script>
+<script src="{{ asset('assets/js/jquery-1.11.0.min.js')}}"></script>
+<script src="{{ asset('assets/js/jquery-migrate-1.2.1.min.js')}}"></script>
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('assets/js/templatemo.js')}}"></script>
+<script src="{{ asset('assets/js/custom.js')}}"></script>
 <!-- End Script -->
 </body>
 
