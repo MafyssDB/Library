a0b21a33e45e20823f14736459b8759d23459306
MafyssDB <hanterhanter599@gmail.com>
2023-12-13 15:32:42 +0700

    update laravel

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,52 @@
+document.getElementById("bodyWhite").onclick = function() {
+        document.getElementById("body").className = "body__background_white";
+    }
+
+document.getElementById("bodyGrey").onclick = function() {
+        document.getElementById("body").className = "body__background_grey";
+    }    
+
+document.getElementById("bodyBlack").onclick = function() {
+        document.getElementById("body").className = "body__background_black";
+    }    
+
+document.getElementById("bodyYellow").onclick = function() {
+        document.getElementById("body").className = "body__background_yellow";
+    } 
+
+
+
+document.getElementById("fontWhite").onclick = function() {
+        document.getElementById("font").className = "font__color_white";
+    } 
+
+document.getElementById("fontGrey").onclick = function() {
+        document.getElementById("font").className = "font__color_grey";
+    } 
+
+document.getElementById("fontBlack").onclick = function() {
+        document.getElementById("font").className = "font__color_black";
+    } 
+
+document.getElementById("fontYellow").onclick = function() {
+        document.getElementById("font").className = "font__color_yellow";
+    }                            
+
+
+
+
+document.getElementById("fontSize1").onclick = function() {
+        document.getElementById("size").className = "font__size_small";
+    }
+
+document.getElementById("fontSize2").onclick = function() {
+        document.getElementById("size").className = "font__size_medium";
+    }
+
+document.getElementById("fontSize3").onclick = function() {
+        document.getElementById("size").className = "font__size_big";
+    }
+
+document.getElementById("fontSize4").onclick = function() {
+        document.getElementById("size").className = "font__size_huge";
+    }    
